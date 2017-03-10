2e14a946cc79dcac0d61caf853ee244475b117b6
Liam <liam@liamcarr.com>
2017-03-10 09:03:37 -0500

    Removed the employeeMessengerListActivity and replaced it with the ability to long press on a table name and select multiple options, The messenger being one of those options. Removed the MessengerActivity. Since there was no need for a whole screen dedicated to only displaying a waiter's name, the messenger was implemented in a more streamline manner. Once the messenger button is pressed, the app is redirected instantly to the chat view.

--- app/src/main/java/com/example/carrc/seniorproject/CustomerDashboardActivity.java
+++ app/src/main/java/com/example/carrc/seniorproject/CustomerDashboardActivity.java
@@ -1,25 +1,63 @@
 package com.example.carrc.seniorproject;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.List;
 
 public class CustomerDashboardActivity extends AppCompatActivity {
 
+    // saves the current waiter's name who is working the table that is saved in the apps shared preferences.
+    String waiterName;
+
+    public void locateWaiter(){
+        // locate the table number from shared preferences and create the string for the table name
+        // as it is saved in the Tables database
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.carrc.seniorproject", Context.MODE_PRIVATE );
+        String tableNumber = sharedPreferences.getString("tableNumber", "");
+        String formatedName = "table" + tableNumber;
+
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Tables");
+        query.whereEqualTo("name", formatedName);
+
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if(e == null && objects.size() > 0){
+                    waiterName = objects.get(0).getString("staff");
+                }
+            }
+        });
+    }
+
     public void messenger(View view){
-        Intent intent = new Intent(this, MessengerListActivity.class);
+
+        // creates the chat activity between the current waiter and the table number
+        Intent intent = new Intent(getApplicationContext(), ChatActivity.class);
+        intent.putExtra("staffName", waiterName);
         startActivity(intent);
+
     }
 
     public void openMenu(View view){
-        Intent intent = new Intent(this, MenuMainActivity.class);
-        startActivity(intent);
+        //Intent intent = new Intent(this, MenuMainActivity.class);
+        //startActivity(intent);
     }
 
     @Override
@@ -52,5 +90,8 @@ public class CustomerDashboardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_customer_dashboard);
 
         setTitle("Customer Dashboard");
+
+        // query the name of the waiter currently working the apps saved table number
+        locateWaiter();
     }
 }
