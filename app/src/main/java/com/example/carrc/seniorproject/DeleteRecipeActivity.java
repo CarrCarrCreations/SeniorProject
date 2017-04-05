aa0ac790dc133710b0c33cfe4646137f49a1e082
Scott <scottpaillant@gmail.com>
2017-04-05 14:58:58 -0400

    Delete layout added

--- app/src/main/java/com/example/carrc/seniorproject/DeleteRecipeActivity.java
+++ app/src/main/java/com/example/carrc/seniorproject/DeleteRecipeActivity.java
@@ -1,10 +1,27 @@
 package com.example.carrc.seniorproject;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by scottieb on 4/5/17.
  */
 
 public class DeleteRecipeActivity extends AppCompatActivity {
+
+    public void deleteRecipe(View view){
+        //get recipe in search view
+
+        //ask for confirmation
+
+        //delete from database
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_recipe);
+    }
 }
